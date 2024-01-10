3aba375786571f5fc8e3c8d910540fa259bdf2b4
J Vance <justinleevance94@gmail.com>
2024-01-10 17:39:17 -0500

    done

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -2,7 +2,7 @@
 function MessageList ({messages}) {
     const mappedMessages = messages.map((messObj) => {
         return (
-            <li className="message sent">
+            <li className="message {messObj.type}" >
                 <div className="icon">{messObj.name.slice(0,1)}</div>
                 <span className="content">{messObj.content}</span>
              </li>
